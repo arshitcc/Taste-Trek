c22408f074c5b7bf0e1d0561bf3e8d396c38943b
arshitcc <arshitcc@gmail.com>
2024-12-27 01:57:59 +0530

    chore(vite) : update vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,21 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { loadEnv } from "vite"
 
-export default defineConfig({
-  server : {
-    proxy : {
-      '/api' : "http://localhost:6969"
-    }
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      proxy: {
+        '/api': env.VITE_API_URL,
+      }
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+      },
+    },
+  }
 })
